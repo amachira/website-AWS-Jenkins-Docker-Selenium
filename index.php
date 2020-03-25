17160a4ed292534f85f0ed9e7762b28e75c4c225
Anagesh <amachira@hawk.iit.edu>
2020-03-25 16:28:41 +0000

    source folder

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+<link href="//db.onlinewebfonts.com/c/a4e256ed67403c6ad5d43937ed48a77b?family=Core+Sans+N+W01+35+Light" rel="stylesheet" type="text/css"/>
+<link rel="stylesheet" href="form.css" type="text/css">
+</head>
+<body>
+<div class="body-content">
+  <div class="module">
+    <h1>Create an account</h1>
+    <form class="form" action="form.php" method="post" enctype="multipart/form-data" autocomplete="off">
+    
+      <input type="text" placeholder="User Name" name="username" />
+      <input type="email" placeholder="Email" name="email"/>
+      <input type="password" placeholder="Password" name="password" autocomplete="new-password"/>
+      <input type="password" placeholder="Confirm Password" name="confirmpassword" autocomplete="new-password"/>
+<input type="button"   class="btn btn-block btn-primary" onclick="window.location.href = '/welcome.php';" value="Register"/>
+
+</form>
+  </div>
+</div>
+</body>
+</html>
---
+++ src/welcome.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	    <head>
+		          <meta charset="utf-8">
+			          <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+				        <title>WELCOME</title>
+					        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+						        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+							        <style>body {margin-top: 40px; background-color: #26A860;}</style>
+								        <link href="assets/css/bootstrap-responsive.min.css" rel="stylesheet">
+									        <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+										    </head>
+
+  <body>
+
+<a href="index.php">Logout</a>
+        <div class="container">
+            <div class="hero-unit">
+
+                </p>
+				
+           </div>
+        </div>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+    </body>
+
+										        <body>
+												       <div class="container"
+			            <div class="hero-unit">
+					                  <h1></h1>
+							                  <h1>Welcome Anageshwar to your AWS cloud website</h1>
+									                  
+											                  <p>Anageshwar
+													  		
+															                    
+																	                    
+																				                 </p>
+																						                 <?php
+		                            $myfile = fopen("/var/www/my-vol/date", "r") or die("");
+		                            echo fread($myfile,filesize("/var/www/my-vol/date"));
+		                            fclose($myfile);
+		                    ?>
+				    <img src="http://img02.deviantart.net/e3e0/i/2015/072/c/b/team_miracle_workers_logo_by_socalledartist-d8liyvr.jpg" alt="W3Schools.com">
+				                </div>
+						        </div>
+
+															           <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+																           <script src="assets/js/bootstrap.min.js"></script>
+																	   <img src="/images/image1.png" width="280" height="125" title="Logo of a company" alt="Logo of a company" />
+																	   <img src"http://img02.deviantart.net/e3e0/i/2015/072/c/b/team_miracle_workers_logo_by_socalledartist-d8liyvr.jpg" alt="W3Schools.com">
+																	       </body>
+
+</html>
